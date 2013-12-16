00d47f0c21bd55d5936e144d812b074f2296f554
gorkaelgezabal <gorkaelgezabal@gmail.com>
2013-12-16 18:17:35 +0100

    connection to tracker

--- SSDD_BitTorrentClient/src/es/deusto/ingenieria/ssdd/bitTorrent/Client/Client.java
+++ SSDD_BitTorrentClient/src/es/deusto/ingenieria/ssdd/bitTorrent/Client/Client.java
@@ -12,9 +12,15 @@ import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Map.Entry;
 
+import es.deusto.ingenieria.ssdd.bitTorrent.bencoding.Bencoder;
+import es.deusto.ingenieria.ssdd.bitTorrent.bencoding.Index;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.MetainfoFile;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.handler.MetainfoFileHandler;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.handler.SingleFileHandler;
@@ -52,27 +58,40 @@ public class Client {
 
 			InetAddress ip = InetAddress.getLocalHost();
 			
-			String announce = handler.getMetainfo().getAnnounce()+"?info_hash="+metaInfo.getInfo().getUrlInfoHash()+"&peer_id="+peer_id+"&port="+PORT+"&uploaded=0&downloaded=0&left=0&ip="+ip.getHostAddress()+"&event=started";
-			
+//			String announce = handler.getMetainfo().getAnnounce()+"?info_hash="+metaInfo.getInfo().getUrlInfoHash()+"&peer_id="+peer_id+"&port="+PORT+"&uploaded=0&downloaded=0&left=0&ip="+ip.getHostAddress()+"&event=started";
+			String announce = handler.getMetainfo().getAnnounce()+"?info_hash="+metaInfo.getInfo().getUrlInfoHash()+"&peer_id="+peer_id+"&port="+PORT+"&uploaded=0&downloaded=0&left=16384&event=started";
+
 			System.out.println("ANNOUNCE"+announce);
 			URL url = new URL(announce);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			System.out.println("Request sent");
+			String USER_AGENT = "Mozilla/5.0"; 
 			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", USER_AGENT);
 			
-			InputStream is = con.getInputStream(); 
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+	 
+			//print result
+			System.out.println(response.toString());
 			
-			int i;
-			char c;
-			while((i=is.read())!=-1)
-	         {
-	            // converts integer to character
-	            c=(char)i;
-	            
-	            // prints character
-	            System.out.print(c);
-	         }
-			
+//			Bencoder bencoder = new Bencoder();
+//			HashMap<String, Object> unbencodedMap = bencoder.unbencodeDictionary(response);
+//			Iterator<Entry<String, Object>> it = unbencodedMap.entrySet().iterator();
+//			Entry<String, Object> entry = null;
+//			
+//			while (it.hasNext()) {
+//				entry = it.next();
+//				System.out.println("   * Key: '" + entry.getKey() + "' - Value: '" + entry.getValue().toString() + "'");
+//			}
+//		
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
