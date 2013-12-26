a818395a961937ae77c5c8b280bf107698838c76
gorkaelgezabal <gorkaelgezabal@gmail.com>
2013-12-26 21:48:46 +0100

    First parsing

--- SSDD_BitTorrentClient/src/es/deusto/ingenieria/ssdd/bitTorrent/Client/Client.java
+++ SSDD_BitTorrentClient/src/es/deusto/ingenieria/ssdd/bitTorrent/Client/Client.java
@@ -35,6 +35,7 @@ import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.MetainfoFile;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.handler.MetainfoFileHandler;
 import es.deusto.ingenieria.ssdd.bitTorrent.metainfo.handler.SingleFileHandler;
 import es.deusto.ingenieria.ssdd.bitTorrent.peer.protocol.messages.Handsake;
+import es.deusto.ingenieria.ssdd.bitTorrent.peer.protocol.messages.PeerProtocolMessage;
 import es.deusto.ingenieria.ssdd.bitTorrent.util.StringUtils;
 import es.deusto.ingenieria.ssdd.bitTorrent.util.ToolKit;
 
@@ -43,187 +44,200 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int PORT = 3333;
+		
 		try {
 			MetainfoFileHandler<?> handler;
-			
+
 			String torrentDir ="C:/Users/Nirie/Desktop/ar.docx.torrent";
 			File torrent = new File(torrentDir);
 			handler = new SingleFileHandler();
 			System.out.println(torrent.getPath());
 			handler.parseTorrenFile(torrent.getPath());
-			
-			MetainfoFile<?> metaInfo = handler.getMetainfo();
-			
-			
-			
-			
-/////////////   PETICION AL TRACKER ////////////////////////////////			
 
-			String peer_id = ToolKit.generatePeerId();
-			
-			
-			
-			
-			System.out.println("hash not encoded"+metaInfo.getInfo());
-			 
-			
+			MetainfoFile<?> metaInfo = handler.getMetainfo();
+
+
+
+
+			/////////////   REQUEST TO TRACKER ////////////////////////////////			
+
+			String localPeerId = ToolKit.generatePeerId();
+
 			InetAddress ip = InetAddress.getLocalHost();
-			
-			String response = notifyTracker(handler.getMetainfo(),peer_id,Integer.toString(PORT),"0","0","16384","started");
-	 			
+
+			String response = notifyTracker(handler.getMetainfo(),localPeerId,Integer.toString(PORT),"0","0","16384","started");
+
 			System.out.println(response.toString());
-			
+
 			Bencoder bencoder = new Bencoder();
 			HashMap<String, Object> unbencodedMap = bencoder.unbencodeDictionary(String.valueOf(response).getBytes());
 			Iterator<Entry<String, Object>> it = unbencodedMap.entrySet().iterator();
 			Entry<String, Object> entry = null;
-			
+
 			while (it.hasNext()) {
 				entry = it.next();
 				System.out.println("   * Key: '" + entry.getKey() + "' - Value: '" + entry.getValue().toString() + "'");
 			}
+
 			
+			//GET PEER LIST
 			ArrayList<Object> peerListUnbencoded = (ArrayList<Object>)unbencodedMap.get("peers");
-			
+
 			ArrayList<Peer> peerList = new ArrayList<Peer>();
 			for (int i=0; i<peerListUnbencoded.size();i++){
-				Peer currentPeer = new Peer();
-				
+					
 				//Get current peer info
 				HashMap<String, Object> currentHash= (HashMap<String, Object>) peerListUnbencoded.get(i);
 				
 				//Get ip
 				InetAddress currentIp = InetAddress.getByName((String)currentHash.get("ip"));
-				System.out.println("vla"+currentIp);
-				currentPeer.setIp(currentIp);
-				
 				//Get port
 				Integer port =(Integer) currentHash.get("port");
-				currentPeer.setPort(port);
 				
-				//Get peer id
-				String peerId =(String) currentHash.get("peer id");
-				currentPeer.setId(peerId);
+
+				//Check if peer is localhost
+				if(!port.equals(PORT) || !port.equals(PORT)){
+					Peer currentPeer = new Peer();
+					currentPeer.setIp(currentIp);			
+					currentPeer.setPort(port);
+
+					//Get peer id
+					String peerId =(String) currentHash.get("peer id");
+					currentPeer.setId(peerId);
+
+					peerList.add(currentPeer);
+				}
 				
-				peerList.add(currentPeer);
 			}
+
 			
-			
-			//PRUEBA:  Se manda un handsake al primer peer que no sea yo
-			
-//			Peer currentPruebaPeer = null;
-//			Peer pruebaPeer = null;
-//			for(int i=0; i<peerList.size();i++){
-//				currentPruebaPeer = peerList.get(0);
-//				boolean t = currentPruebaPeer.getIp().isAnyLocalAddress();
-//				boolean r = currentPruebaPeer.getIp().isLoopbackAddress();
-////				if (!currentPruebaPeer.getIp().isAnyLocalAddress()&&!currentPruebaPeer.getIp().isLoopbackAddress()){
-//				System.out.println(currentPruebaPeer.getPort()+" is equal 39402");
-//				if(currentPruebaPeer.getPort() == 39402){	
-//					pruebaPeer=currentPruebaPeer;
-//				}
-//				
-//			}
-			
-			
-			System.out.println("Connecting to "+InetAddress.getLocalHost()+":"+39402);
-			Socket socket = new Socket(InetAddress.getLocalHost(),39402 );
-			
-			
+			//SEND HANDSAKE
+			//Para probar se manda la peticion al primero de la lista
+			System.out.println("Connecting to "+peerList.get(0).getIp()+":"+peerList.get(0).getPort());
+			Socket socket = new Socket(peerList.get(0).getIp(),peerList.get(0).getPort() );
+
 			DataOutputStream socketOutputStream = new DataOutputStream(socket.getOutputStream());   
-			
+
 			Handsake handsake = new Handsake();
-			
+
 			byte[] hash = metaInfo.getInfo().getInfoHash();
-			
-			
+			String peerId = peerList.get(0).getId();
+
 
 			handsake.setInfoHash(new String(hash));
-			handsake.setPeerId(peer_id);
+			handsake.setPeerId(peerId);
 
-			
+
 			byte[] handsakeBytes=handsake.getBytes();
 			int handshakeLenght = handsakeBytes.length;
-			
+
 			if(handshakeLenght != 68){
 				System.out.println("Handshake lenght incorrect");
 			}
 			socketOutputStream.write(handsakeBytes);
 
 			System.out.println("Handsake sended:"+handsake.toString());
+			
+			
+			
 			//Se recive el mensaje
-			      
-	         
-	         DataInputStream in = new DataInputStream(socket.getInputStream());
-
-	         byte [] prueba2 = new byte[300];
-	         
-	         in.read(prueba2);
 
 
-			 System.out.println(new String(prueba2));
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+
+			byte [] responseHandsake = new byte[68];
+			in.read(responseHandsake);
+			System.out.println(new String(responseHandsake));
+
+			int availableBytes = in.available();
+			byte [] responseBytes = new byte[availableBytes];
+			in.read(responseBytes);
+
+			System.out.println(new String(responseBytes));
+			
+//			SE PARSEA EL MENSAJE
+			System.out.println("Name length"+new Integer(responseHandsake[0]));
+			
+			byte [] hashBytes = Arrays.copyOfRange(responseHandsake, 28, 48);
+			System.out.println("Info hash recivido"+new String(hashBytes));
+			
+//			Se comprueba que tenemos el mismo fichero comparando el hash de respuesta con nuestro hash
+			if(compareHash(new String(hashBytes),new String(hash))){
+				System.out.println("Hash checked");
+			}
+			
+			
+
+
+			
+
 			socketOutputStream.close();
 			socket.close();
-//		
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-		
-	}
-	
-	
-	public static String notifyTracker (MetainfoFile<?> metaInfo,String peer_id, String port, String uploaded, String downloaded, String left, String event){
-		
-		
-		System.out.println("hash not encoded"+metaInfo.getInfo());
-		 
+				//		
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
 
-		try {
-			InetAddress ip = InetAddress.getLocalHost();
+		}
 
-			String announce = metaInfo.getAnnounce()+"?info_hash="+metaInfo.getInfo().getUrlInfoHash()+"&peer_id="+peer_id+"&port="+port+"&uploaded="+uploaded+"&downloaded="+downloaded+"&left="+left+"&event="+event;
 
-			System.out.println("ANNOUNCE"+announce);
-			URL url = new URL(announce);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			System.out.println("Request sent");
-			String USER_AGENT = "Mozilla/5.0"; 
-			con.setRequestMethod("GET");
-			con.setRequestProperty("User-Agent", USER_AGENT);
-			
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-	 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
+		public static String notifyTracker (MetainfoFile<?> metaInfo,String peer_id, String port, String uploaded, String downloaded, String left, String event){
+
+
+			System.out.println("hash not encoded"+metaInfo.getInfo());
+
+
+			try {
+				InetAddress ip = InetAddress.getLocalHost();
+
+				String announce = metaInfo.getAnnounce()+"?info_hash="+metaInfo.getInfo().getUrlInfoHash()+"&peer_id="+peer_id+"&port="+port+"&uploaded="+uploaded+"&downloaded="+downloaded+"&left="+left+"&event="+event;
+
+				System.out.println("ANNOUNCE"+announce);
+				URL url = new URL(announce);
+				HttpURLConnection con = (HttpURLConnection) url.openConnection();
+				System.out.println("Request sent");
+				String USER_AGENT = "Mozilla/5.0"; 
+				con.setRequestMethod("GET");
+				con.setRequestProperty("User-Agent", USER_AGENT);
+
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+
+				return String.valueOf(response);
+
+
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			in.close();
-			
-			return String.valueOf(response);
-			
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			return null;
+
 		}
 		
-		return null;
-		
-	}
+		public static boolean compareHash(String hash1, String hash2){
+			
+			return hash1.equals(hash2);
+		}
 
-}
+	}
